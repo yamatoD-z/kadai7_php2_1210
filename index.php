5c1f25d6d79c20e85ce618674d5403464ab31c30
JJTTs <jungletsukiyo@gmail.com>
2022-12-14 00:48:55 +0900

    set

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>データ登録</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <style>
+    div {
+        padding: 10px;
+        font-size: 16px;
+    }
+    </style>
+</head>
+
+<body>
+
+    <!-- Head[Start] -->
+    <header>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header"><a class="navbar-brand" href="select.php">データ一覧</a></div>
+            </div>
+        </nav>
+    </header>
+    <!-- Head[End] -->
+
+    <!-- Main[Start] -->
+    <form method="post" action="insert.php">
+        <div class="jumbotron">
+            <fieldset>
+                <legend>英文を入力</legend>
+                <label><textArea name="sentence" rows="50" cols="60">
+                    <?php echo file_get_contents('text.txt')?>
+                    </textArea></label><br>
+                <label>出展：<input type="text" name="source" value="Influence"></label><br>
+                <input type="submit" value="送信">
+            </fieldset>
+        </div>
+    </form>
+    <!-- Main[End] -->
+
+
+</body>
+
+</html>
---
+++ insert.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * 1. index.phpのフォームの部分がおかしいので、ここを書き換えて、
+ * insert.phpにPOSTでデータが飛ぶようにしてください。
+ * 2. insert.phpで値を受け取ってください。
+ * 3. 受け取ったデータをバインド変数に与えてください。
+ * 4. index.phpフォームに書き込み、送信を行ってみて、実際にPhpMyAdminを確認してみてください！
+ */
+
+//1. POSTデータ取得
+$sentence = $_POST['sentence'];
+$source = $_POST['source'];
+
+//２. 単語数カウントの処理
+// A.　すべて小文字
+$smallSentence=strtolower($sentence);
+// echo $smallsentence;
+
+// B.一般語を削除（キリがなくなったので記号だけ）
+$smallSentence=str_replace(':', "", $smallSentence);
+$smallSentence=str_replace(';', "", $smallSentence);
+$smallSentence=str_replace('"', "", $smallSentence);
+$smallSentence=str_replace('?', "", $smallSentence);
+$smallSentence=str_replace('!', "", $smallSentence);
+$smallSentence=str_replace("'", "", $smallSentence);
+$smallSentence=str_replace("(", "", $smallSentence);
+$smallSentence=str_replace(")", "", $smallSentence);
+$smallSentence=str_replace("/", "", $smallSentence);
+$smallSentence=str_replace(".", "", $smallSentence);
+$smallSentence=str_replace(",", "", $smallSentence);
+$smallSentence=str_replace("“", "", $smallSentence);
+$smallSentence=str_replace("”", "", $smallSentence);
+
+
+
+// C.　配列化
+$wordarray = explode(" ",$smallSentence);
+// print_r($array);
+
+// D.配列内の同じ要素をカウント
+$countarray = array_count_values($wordarray);
+// print_r($countarray);
+
+// $word=[];
+// $count=[];
+// foreach($countarray as $key => $value){
+//     $word .=$key;
+//     $count .=$value;
+// }
+
+// print_r($word);
+// print_r($value);
+
+//2. DB接続します
+try {
+    //ID:'root', Password: xamppは 空白 ''
+    $pdo = new PDO('mysql:dbname=eigo;charset=utf8;host=localhost', 'root', '');
+} catch (PDOException $e) {
+    exit('DBConnectError:'.$e->getMessage());
+}
+
+// 一旦、中身を空にする（今回、同じ英文を操作しているので）
+$pdo->exec('TRUNCATE TABLE tangocount');
+
+//３．データ登録SQL作成
+
+// 1. SQL文を用意
+// 上記の単語と出現回数をSQLに入れていく
+foreach($countarray as $key => $value){
+$stmt = $pdo->prepare("INSERT INTO
+                        tangocount(id, word, count, source, date)
+                        VALUES(NULL, :word, :count, :source, sysdate() )");
+
+//  2. バインド変数を用意
+// Integer 数値の場合 PDO::PARAM_INT
+// String文字列の場合 PDO::PARAM_STR
+$stmt->bindValue(':word', $key, PDO::PARAM_STR);
+$stmt->bindValue(':count', $value, PDO::PARAM_STR);
+$stmt->bindValue(':source', $source, PDO::PARAM_STR);
+
+//  3. 実行
+$status = $stmt->execute();
+
+}
+
+//４．データ登録処理後
+if ($status === false) {
+    //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+    $error = $stmt->errorInfo();
+    exit('ErrorMessage:'.$error[2]);
+} else {
+    //５．index.phpへリダイレクト
+    header('Location: index.php');
+}
